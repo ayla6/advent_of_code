35c21e5418209af19c0eeb008aabdbd7f23847ca
ayla <ayla-git.barcode041@silomails.com>
2025-12-02 18:23:11 -0300

    rust optimisation because i saw a ton of people saying generating stuff beforehand was faster

--- 2025/2/rust/src/main.rs
+++ 2025/2/rust/src/main.rs
@@ -1,7 +1,11 @@
+use std::collections::HashSet;
+
 struct Range {
     start: u64,
     end: u64,
 }
+
+const MAX_LENGTH: usize = 10;
 
 fn main() {
     let input: Vec<Range> = include_str!("../../input.txt")
@@ -19,7 +23,7 @@ fn main() {
         })
         .collect();
 
-    let powers_of_ten: Vec<u64> = (0..10).map(|i| 10_u64.pow(i)).collect();
+    let powers_of_ten: Vec<u64> = (0..=MAX_LENGTH as u32).map(|i| 10_u64.pow(i)).collect();
 
     let part_1 = input.iter().fold(0_u64, |acc, v| {
         (v.start..=v.end).fold(acc, |acc, n| {
@@ -39,19 +43,31 @@ fn main() {
     });
     println!("{}", part_1);
 
+    let mut part_2_invalid: HashSet<u64> = HashSet::new();
+    for len in 1..=MAX_LENGTH / 2 {
+        // 1-9, 10-99, 100-999, 100000-999999
+        for combination in
+            *powers_of_ten.get(len - 1).unwrap_or(&1)..*powers_of_ten.get(len).unwrap()
+        {
+            let mut number = 0;
+            // 0 is just the number (9), 1 is one repetition (99)
+            for repeat in 0..MAX_LENGTH / len {
+                number += combination * powers_of_ten.get((len * repeat) as usize).unwrap();
+                if repeat > 0 {
+                    part_2_invalid.insert(number);
+                }
+            }
+        }
+    }
+    let part_2_invalid = part_2_invalid;
+
     let part_2 = input.iter().fold(0_u64, |acc, v| {
-        (v.start..=v.end).fold(acc, |acc, n| {
-            if n <= 10 {
+        (v.start..=v.end).fold(acc, |acc, num| {
+            if num <= 10 {
                 return acc;
             }
-            let len = (n.ilog10() + 1) as usize;
-            match (1..=len / 2).rfind(|clen| {
-                let num = n % powers_of_ten.get(*clen).unwrap();
-                let res = (0..len / clen)
-                    .fold(0, |acc, i| acc + num * powers_of_ten.get(i * clen).unwrap());
-                res == n
-            }) {
-                Some(_) => acc + n,
+            match part_2_invalid.get(&num) {
+                Some(_) => acc + num,
                 _ => acc,
             }
         })
