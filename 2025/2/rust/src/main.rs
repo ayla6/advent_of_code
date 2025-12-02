8e4c8f548483d3c1ca640c0b358ba29e164b1074
ayla <ayla-git.barcode041@silomails.com>
2025-12-02 13:33:38 -0300

    day 2 2025 in rust  it's so ugly

---
+++ 2025/2/rust/src/main.rs
@@ -0,0 +1,58 @@
+struct Range {
+    start: u64,
+    end: u64,
+}
+
+fn main() {
+    let input: Vec<Range> = std::fs::read_to_string("../input.txt")
+        .expect("invalid input!!")
+        .trim()
+        .split(",")
+        .map(|v| {
+            let range: Vec<&str> = v.split("-").collect();
+            match range[..] {
+                [start, end] => Range {
+                    start: start.parse().expect("invalid number"),
+                    end: end.parse().expect("invalid number"),
+                },
+                _ => panic!("bad input!"),
+            }
+        })
+        .collect();
+
+    let part_1 = &input.iter().fold(0_u64, |acc, v| {
+        (v.start..=v.end).fold(acc, |acc, n| {
+            if n <= 10 {
+                return acc;
+            }
+            let len = n.ilog10() + 1;
+            if len % 2 == 1 {
+                return acc;
+            }
+            let pow = 10_u64.pow(len / 2);
+            if n / pow == n % pow {
+                return acc + n;
+            }
+            acc
+        })
+    });
+    println!("{}", part_1);
+
+    let part_2 = &input.iter().fold(0_u64, |acc, v| {
+        (v.start..=v.end).fold(acc, |acc, n| {
+            if n <= 10 {
+                return acc;
+            }
+            let len = n.ilog10() + 1;
+            match (1..=len / 2).rfind(|clen| {
+                let num = n % 10_u64.pow(*clen);
+                let res = (0..len / clen).fold(0, |acc, i| acc + num * 10_u64.pow(i * clen));
+                res == n
+            }) {
+                Some(_) => acc + n,
+                _ => acc,
+            }
+        })
+    });
+    println!("{}", part_2);
+}
